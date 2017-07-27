3eb7a2acdff63cda3b1aec22b938b7281cc117e1
NOVIKOVVLADIMIR <novikov_v79@mail.ru>
2017-07-27 12:53:08 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,47 +5,24 @@ package ru.stqa.pft.sandbox;
  */
 
 class Point {
-    public double x; // абсцисса точки
-    public double y; // ордината точки
 
-    // возвращает строку с описанием точки
-    public String toString() {
-        return "("+x+";"+y+")";
-    }
-    // выводит на экран описание точки
-    public void printPoint() {
-        System.out.print(this.toString());
-    }
-    // метод перемещает точку на указанный вектор
-    public void movePoint(double a, double b) {
-        x = x + a;
-        y = y + b;
-    }
-    // метод изменяет координаты точки на указанные
-    public void setPoint(double a, double b) {
-        x = a;
-        y = b;
-    }
-    // конструктор по умолчанию, создающий точку в начале координат
-    public Point() {
-        x = 100.0;
-        y = 500.0;
-    }
-    // конструктор, создающий точку с указанными координатами
-    public Point(double a, double b) {
-        x = a;
-        y = b;
-    }
-    // метод вычисляющий расстояние между точками
-    public double length(Point p) {
-        return Math.sqrt( Math.pow(p.x-x,2) + Math.pow(p.y-y,2) );
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+
     }
 
+    double dist(double x2, double y2) {
+        double x0 = x2 - this.x;
+        double y0 = y2 - this.y;
+        return Math.sqrt(Math.pow(x0, 2) + Math.pow(y0, 2));
+    }
 
-    public static void main(String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point(-1,1);
-        System.out.println("Distanse between "+p1+" and "+p2+" equals "+p1.length(p2));
+    public double distance(Point p) {
+        return dist(p.x, p.y);
 
     }
 }
