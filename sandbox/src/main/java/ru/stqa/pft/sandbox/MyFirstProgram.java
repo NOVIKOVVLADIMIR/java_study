574f81e7f104e27b7cfe2646bbc9fb34402f1e9b
NOVIKOVVLADIMIR <novikov_v79@mail.ru>
2017-07-27 12:55:21 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,6 +7,14 @@ public class MyFirstProgram {
         hello("user");
         hello("Vladimir");
 
+        Point p1 = new Point(4, -4);
+        Point p2 = new Point(5, -5);
+
+
+        System.out.println("Расстояние на координатной оси между точками c координатами " + "(" + p1.x + ", "+ p1.y + ")" + " и " + "(" + p2.x + ", " + p2.y + ")" + " = " + p2.distance(p1));
+
+
+
         Square s = new Square(5);
         s.l = 5;
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
